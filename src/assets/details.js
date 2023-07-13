067be50afadca0f89407ee0d3c0d014fca070bc2
kirbygit <kirbyjames.fernandez01@student.csulb.edu>
2023-07-13 16:16:21 -0700

    Update base path found repository

--- src/assets/details.js
+++ src/assets/details.js
@@ -4,7 +4,7 @@ module.exports = {
   university: 'California State University, Long Beach',
   major: 'Computer Science (B.S)',
   resumeLink: '', // See https://tinyurl.com/create-resume-link
-  repoId: 'kirbyfern.github.io',
+  repoId: 'Verify_Kirby',
   profileFileName: 'kirbyf.jpg', // Add image file to assets directory
   gitHubLink: 'https://github.com/kirbyfern',
   linkedInLink: 'https://www.linkedin.com/in/kirbyjfernandez/'

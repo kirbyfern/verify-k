594c3ae1c837e9c7818ebe8f73cbec5f97bc06f1
kirbygit <kirbyjames.fernandez01@student.csulb.edu>
2023-07-13 16:42:53 -0700

    Update dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
-const details = require('./src/assets/details.js');
+
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
